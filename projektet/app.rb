2d13f95ddfa2e7107d25769309e4fee4fa81d15d
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-02-05 14:07:01 +0100

    Update app.rb
    små börjar med ruby

--- projektet/app.rb
+++ projektet/app.rb
@@ -1,3 +1,18 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
+
 #En furum som visar spel i samma genre och företag som andra spel
 #samt me kommentarar på spelen 
 #många i många databaser 
+
+
+
+get('/') do
+    slim(:index)
+  end
+
+
