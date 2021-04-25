b52b14aa2247d642f4430f1b98642b01599dc4a8
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-04-25 18:25:20 +0200

    Update model.rb

--- projektet/model.rb
+++ projektet/model.rb
@@ -1,12 +1,24 @@
 #funktioner för DB databasen 
 module Model
 
+  #Attempts to connect to databasen 
+  # @return [String]
   def connect_to_db()
       db = SQLite3::Database.new('db/Database.db')
       return db 
 
   end 
 
+    # Attempts to create a new user
+    #
+    # @param [Hash] params form data
+    # @option params [String] username The username
+    # @option params [String] password The password
+    # @option params [String] repeat_password The repeated password
+    #   @return [Hash]
+    #   * :error [Boolean] whether an error occured
+    #   * id [Integer] The user's ID if the user was created
+    # @see Model#connect_to_db
   def register_user()
       username = params[:username]
     password = params[:password]
@@ -26,6 +38,14 @@ module Model
 
   end 
 
+  # Attempts to login user
+  #
+  # @param [Hash] params form data
+  # @option params [String] username The username
+  # @option params [String] password The password
+  # 
+  # @return [Integer] The ID of the user
+  # @return [false] if credentials do not match a user
   def login_user()
       username = params[:username]
       password = params[:password]
@@ -44,6 +64,15 @@ module Model
   end 
 
 
+  # Finds all games in the database 
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the title
+  # * :name [String]  The name of the games
+  # * :picture_name [String]  The picture of the game
+  # * :Console [String]  The console of the games
+  # * :devoloper [String]  The devolopers of the games
+  # @see Model#connect_to_db
   def title()
     db = connect_to_db()
     db.results_as_hash = true 
@@ -53,6 +82,12 @@ module Model
     
   end
 
+  # Finds the geners name and relation with the game in the database in a inner join 
+  # @param [Hash] params form data
+  # @option params [Integer] id The title_id (the game we are serhing for genres)
+  # @return [Hash]
+  # * :genres [String] The genres of the games
+  # @see Model#connect_to_db
   def genre_info()
     title_id = params[:id].to_i
     db = connect_to_db()
@@ -65,6 +100,17 @@ module Model
     return result
   end 
 
+  # Finds one game in the database where id match 
+  # @param [Hash] params form data
+  # @option params [Integer] id THE game id 
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the title
+  # * :name [String]  The name of the games
+  # * :picture_name [String]  The picture of the game
+  # * :Console [String]  The console of the games
+  # * :devoloper [String]  The devolopers of the games
+  # @see Model#connect_to_db
   def game_info()
     id = params[:id].to_i
     session[:game_id] = params[:id].to_i
@@ -74,6 +120,16 @@ module Model
     return result
   end 
 
+  # Finds one user info in the database where id match 
+  # @param [Hash] params form data  
+  # @option params [Integer] id THE user id
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the user
+  # * :username [String]  The name of the user
+  # * :pwdigest [String]  The pasword but incrypted
+  # * :role [String]  The role of the user
+  # @see Model#connect_to_db
   def user_info()
     db = connect_to_db()
     db.results_as_hash = true 
@@ -83,6 +139,12 @@ module Model
 
   end 
 
+    # Attempts to insert a new row in the title_user_rel table
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The id of the game
+    # @option params [Integer] session id The id of the user
+    # @see Model#connect_to_db
   def save()
     title_id = params[:id].to_i
     user_id = session[:id].to_i
@@ -91,7 +153,15 @@ module Model
     db.execute("INSERT INTO title_user_rel (title_id,user_id) VALUES (?,?)",title_id,user_id)
     redirect("/login_user/#{user_id}/minsida")
   end 
-
+  # Finds all games that is saved for the user in the database 
+  # @param [Hash] params form data  
+  # @option params [Integer] id THE user id
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the title_user_rel
+  # * :title_id [String]  The name of the games
+  # * :user_id [Integer]  The id of the user
+  # @see Model#connect_to_db
   def saves()
     user_id = session[:id].to_i
     db = connect_to_db()
@@ -103,6 +173,15 @@ module Model
     return result
   end 
 
+
+    # Attempts to delete a row from the title_user_rel table
+    #
+    # @param [Integer] id The id_rel ID
+    # @param [Hash] params form data
+    # @option params [Integer] user_id The id of the user
+    # @option params [Integer] title_id The id of the title
+    #
+    # @see Model#connect_to_db
   def delete_minsdia() 
     id = params[:id].to_i
     user_id = session[:id].to_i
@@ -113,7 +192,13 @@ module Model
 
   end 
 
-
+    # Attempts to insert a new row in the user_list table
+    #
+    # @param [String] game The new game title
+    # @param [Hash] params form data
+    # @option params [Integer] id THE user id
+    #
+    # @see Model#connect_to_db
   def new_list()
     game_title = params[:game]
     user_id = session[:id].to_i
@@ -123,6 +208,16 @@ module Model
     redirect("/login_user/#{user_id}/minsida")
   end 
 
+  # Finds all games that the user want to remeber  in the database 
+  # @param [Hash] params form data  
+  # @option params [Integer] id THE user id
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the user_list
+  # * :game [String]  The name of the game
+  # * :user_id [Integer]  The id of the user
+  #
+  # @see Model#connect_to_db
   def user_links()
     user_id = session[:id].to_i
     db = connect_to_db()
@@ -131,6 +226,14 @@ module Model
     return res
   end 
 
+    # Attempts to delete a row from the user_list table
+    #
+    # @param [Integer] id The user_list ID
+    # @param [Hash] params form data
+    # @option params [Integer] user_id The id of the user
+    # @option params [String] game The name of the game
+    #
+    # @see Model#connect_to_db
   def delete_list()
 
     id = params[:id].to_i
@@ -143,6 +246,14 @@ module Model
 
   end 
 
+    # Attempts to update a row in the user_list table
+    #
+    # @param [Integer] id The user_list ID
+    # @param [Hash] params form data
+    # @option params [Integer] user_id The id of the user
+    # @option params [String] game The name of the game
+    #
+    # @see Model#connect_to_db
   def update()
     title = params[:title]
     id = params[:id].to_i
@@ -153,6 +264,19 @@ module Model
     redirect("/login_user/#{user_id}/minsida")
   end 
 
+  # Finds all info about the game that match the id in the database 
+  #
+  # @param [Integer] id The user_list ID
+  # @param [Hash] params form data
+  # @option params [Integer] user_id The id of the user
+  # @option params [String] game The name of the game
+  #
+  # @return [Hash]
+  # * :id [Integer]  The ID of the user_list
+  # * :game [String]  The name of the game
+  # * :user_id [Integer]  The id of the user
+  #
+  # @see Model#connect_to_db
   def edit()
     id = params[:id].to_i
     db = connect_to_db()
